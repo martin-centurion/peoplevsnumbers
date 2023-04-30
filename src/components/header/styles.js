024305bfba55bf494e735bd1f963513944b8a5f9
martin-centurion <martinalejandrocenturion@gmail.com>
2023-04-29 23:36:33 -0300

    [feature]create a header component and module for component and implement in start screen

---
+++ src/components/header/styles.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    height: 80,
+    backgroundColor: '#355070',
+    marginTop: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+});
