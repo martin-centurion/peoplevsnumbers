bbc451e0c2cd6de5b4784c8ed76e3e562ddf1305
martin-centurion <martinalejandrocenturion@gmail.com>
2023-04-30 09:18:12 -0300

    [feature] create card component and layout for start screen

--- src/screens/start-game/index.jsx
+++ src/screens/start-game/index.jsx
@@ -1,12 +1,21 @@
-import { View } from 'react-native';
+import { Button, Text, TextInput, View } from 'react-native';
 
 import { styles } from './styles';
-import { Header } from '../../components/index';
+import { Card, Header } from '../../components/index';
 
 const StartGame = () => {
   return (
     <View style={styles.container}>
       <Header title="Welcome" />
+      <Text style={styles.title}>Start Game</Text>
+      <Card style={styles.inputContainer}>
+        <Text style={styles.label}>Write a Number</Text>
+        <TextInput placeholder="0" style={styles.input} />
+        <View style={styles.buttonContainer}>
+          <Button title="Reset" onPress={() => {}} color="#355070" />
+          <Button title="Confirm" onPress={() => {}} color="#6D597A" />
+        </View>
+      </Card>
     </View>
   );
 };
--- src/screens/start-game/styles.js
+++ src/screens/start-game/styles.js
@@ -5,4 +5,36 @@ export const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'fff',
   },
+  title: {
+    fontSize: 20,
+    color: '#212121',
+    textAlign: 'center',
+    paddingVertical: 20,
+  },
+  inputContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginHorizontal: 20,
+  },
+  label: {
+    fontSize: 14,
+    color: '#000000',
+    paddingVertical: 5,
+    textAlign: 'center',
+  },
+  input: {
+    //width: '100%',
+    borderBottomColor: '#355070',
+    borderBottomWidth: 2,
+    minWidth: 70,
+    fontSize: 20,
+    textAlign: 'center',
+    paddingVertical: 10,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    width: '80%',
+    justifyContent: 'space-around',
+    marginTop: 20,
+  },
 });
